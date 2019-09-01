252489c3623b67e661795ab6aa7dcf5e98537c0a
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-09-01 17:06:45 +0900

    Default file.

---
+++ src/Sample.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+class Sample {
+	public static void main(String[] args) {
+		LocalDateTime localtime = LocalDateTime.now();
+		System.out.println(localtime);
+	}
+}
